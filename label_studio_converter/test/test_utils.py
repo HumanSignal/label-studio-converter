413d71be896e998b68888f5337b1acdda2894216
Tze Yiing <tzeyiing@gmail.com>
2020-05-06 18:49:52 +0800

    added test suite

---
+++ label_studio_converter/test/test_utils.py
@@ -0,0 +1,17 @@
+import unittest
+import ..utils as utils
+class TestUtils(unittest.TestCase):
+
+    def (self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
