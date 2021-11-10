bbdfb37ac86eab88db72e2d4107371890bea0a0a
makseq-ubnt <makseq@gmail.com>
2021-11-11 02:01:10 +0300

    Fix import for setup.py

--- label_studio_converter/__init__.py
+++ label_studio_converter/__init__.py
@@ -1,3 +1,8 @@
-from .converter import Converter
+# try is used here to import this file in setup.py and don't break the setup process
+try:
+    from .converter import Converter
+except ModuleNotFoundError as e:
+    print(e)
+
 
 __version__ = '0.0.33rc5'
