683aa8f1c2b95203d627499f9c2da8fe1c161369
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-05-02 21:14:39 +0300

    fix: DEV-2095: Fix runtime error when duration is not extracted

--- label_studio_converter/audio.py
+++ label_studio_converter/audio.py
@@ -21,11 +21,12 @@ def convert_to_asr_json_manifest(input_data, output_dir, data_key, project_dir, 
             try:
                 audio_path = download(audio_path, output_audio_dir, project_dir=project_dir, upload_dir=upload_dir,
                                       return_relative_path=True, download_resources=download_resources)
+                duration = get_audio_duration(os.path.join(output_audio_dir, os.path.basename(audio_path)))
             except:
                 logger.error('Unable to download {image_path}. The item {item} will be skipped'.format(
                     image_path=audio_path, item=item
                 ), exc_info=True)
-            duration = get_audio_duration(os.path.join(output_audio_dir, os.path.basename(audio_path)))
+                continue
 
             for texts in iter(item['output'].values()):
                 if len(texts) > 0 and 'text' in texts[0]:
